dc6abd22f0ae704364f7bb5aa77dc9667d75a699
haribalaji938 <82193403+haribalaji938@users.noreply.github.com>
2021-08-22 18:23:12 +0530

    created by hari
    This program shows the output of access the elements of an array using pointers.

---
+++ prgm.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[4];
+	cout<<"Enter elements : ";
+	for(int i=0;i<=4;i++)
+	{
+	cin>>a[i];
+    }
+    cout<<"You entered :";
+    for(int i=0;i<=4;i++)
+    {
+    cout<<*(a+i)<<endl;
+	}
+return 0;
+
+}	
+	
